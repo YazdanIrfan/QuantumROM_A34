07df335cbf997203e3d82a655e878209b4f2bbd7
Yazdan Irfan <115693797+YazdanIrfan@users.noreply.github.com>
2026-05-12 13:33:56 +0530

    Change HWUI renderer from skiagl to skiavk
    Switched Renderer to Vulkan to fix issue with Black Screen in Recents

--- scripts/QuantumRom.sh
+++ scripts/QuantumRom.sh
@@ -1691,7 +1691,7 @@ APPLY_CUSTOM_FEATURES() {
     BUILD_PROP "$EXTRACTED_FIRM_DIR" "system" "ro.product.locale" "en-US"
     BUILD_PROP "$EXTRACTED_FIRM_DIR" "system" "wifi.interface" "wlan0"
     BUILD_PROP "$EXTRACTED_FIRM_DIR" "system" "wlan.wfd.hdcp" "disabled"
-    BUILD_PROP "$EXTRACTED_FIRM_DIR" "system" "debug.hwui.renderer" "skiagl"
+    BUILD_PROP "$EXTRACTED_FIRM_DIR" "system" "debug.hwui.renderer" "skiavk"
 	BUILD_PROP "$EXTRACTED_FIRM_DIR" "system" "ro.telephony.sim_slots.count" "2"
     BUILD_PROP "$EXTRACTED_FIRM_DIR" "system" "ro.surface_flinger.protected_contents" "true"
     BUILD_PROP "$EXTRACTED_FIRM_DIR" "system" "persist.audio.voip.enabled" "true"
@@ -1930,7 +1930,7 @@ debug.sf.latch_unsignaled=1
 debug.sf.enable_hwc_vds=1
 
 # HWUI balance (lower GPU stress)
-debug.hwui.renderer=skiagl
+debug.hwui.renderer=skiavk
 debug.hwui.target_cpu_time_percent=60
 debug.hwui.target_gpu_time_percent=65
 
