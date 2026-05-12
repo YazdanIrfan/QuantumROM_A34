51dd1e7369ebc3c29a596b117aeba0c43b3b3a9c
Yazdan Irfan <115693797+YazdanIrfan@users.noreply.github.com>
2026-05-12 13:28:10 +0530

    Change debug.hwui.renderer from skiavk to skiagl to fix Black screen in Recent Apps meniu

--- scripts/QuantumRom.sh
+++ scripts/QuantumRom.sh
@@ -1691,7 +1691,7 @@ APPLY_CUSTOM_FEATURES() {
     BUILD_PROP "$EXTRACTED_FIRM_DIR" "system" "ro.product.locale" "en-US"
     BUILD_PROP "$EXTRACTED_FIRM_DIR" "system" "wifi.interface" "wlan0"
     BUILD_PROP "$EXTRACTED_FIRM_DIR" "system" "wlan.wfd.hdcp" "disabled"
-    BUILD_PROP "$EXTRACTED_FIRM_DIR" "system" "debug.hwui.renderer" "skiavk"
+    BUILD_PROP "$EXTRACTED_FIRM_DIR" "system" "debug.hwui.renderer" "skiagl"
 	BUILD_PROP "$EXTRACTED_FIRM_DIR" "system" "ro.telephony.sim_slots.count" "2"
     BUILD_PROP "$EXTRACTED_FIRM_DIR" "system" "ro.surface_flinger.protected_contents" "true"
     BUILD_PROP "$EXTRACTED_FIRM_DIR" "system" "persist.audio.voip.enabled" "true"
